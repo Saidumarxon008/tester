97555a7418ea16cf1f6cb9ea85a703e789f2cc0b
Lenovo <sa1dumarxon.od1lovv@gmail.com>
2023-12-22 20:57:03 +0500

    A

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tester/theme/constant.dart';
 import 'package:tester/theme/theme.dart';
@@ -11,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -24,9 +24,98 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         decoration: BoxDecoration(
-            gradient: RadialGradient(
-                colors: themeProvider.themeMode().gradientColors!)),
-
+          gradient: RadialGradient(
+              radius: 1,
+              center: const Alignment(-0.8, -0.3),
+              colors: ThemeProvider(isLightTheme: true).themeMode().gradientColors!),
+        ),
+        child: Column(
+            children: [
+              SafeArea(
+                  child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      DateTime.now().hour.toString(),
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                    SizedBox(
+                      width: size.width * .2,
+                      child: const Divider(
+                        height: 0,
+                        thickness: 1,
+                        color: AppColors.white,
+                      ),
+                    ),
+                    Text(
+                      DateTime.now().minute.toString(),
+                      style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                            color: AppColors.white,
+                          ),
+                    ),
+                    // const Spacer(),
+                    const Text(
+                      "Light Dark\nPersonal\nSwitch",
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                      // const Spacer(),
+                      Container(
+                      width: size.width * .2,
+                      height: size.width * .2,
+                      decoration: BoxDecoration(
+                        color: ThemeProvider(isLightTheme: true)
+                            .themeMode()
+                            .switchColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Icon(
+                        Icons.nights_stay_outlined,
+                        size: 50,
+                        color: AppColors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * .2,
+                      child: const Divider(
+                        thickness: 1,
+                        color: AppColors.white,
+                      ),
+                    ),
+                    Text(
+                      "30\u00B0C",
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium!
+                          .copyWith(color: AppColors.white),
+                    ),
+                    Text(
+                      "Clear",
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(color: AppColors.white),
+                    ),
+                    Text(
+                      DateFormat("EEEE").format(DateTime.now()),
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(color: AppColors.white),
+                    ),
+                    Text(
+                      DateFormat("MMM  d").format(DateTime.now()),
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(color: AppColors.white),
+                    ),
+                  ],
+                ),
+              )),
+            ]),
       ),
     );
   }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'constant.dart';
@@ -9,27 +10,23 @@ class ThemeProvider with ChangeNotifier {
 
   ThemeProvider({required this.isLightTheme});
 
-    getCurrentNavigationColor() {
+  getCurrentNavigationColor() {
     if (isLightTheme) {
-      SystemChrome.setSystemUIOverlayStyle(
-          const SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            statusBarBrightness: Brightness.light,
-            statusBarIconBrightness: Brightness.dark,
-            systemNavigationBarColor: Colors.white,
-            systemNavigationBarIconBrightness: Brightness.dark,
-          )
-      );
+      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.white,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ));
     } else {
-      SystemChrome.setSystemUIOverlayStyle(
-          const SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            statusBarBrightness: Brightness.dark,
-            statusBarIconBrightness: Brightness.light,
-            systemNavigationBarColor: Colors.black,
-            systemNavigationBarIconBrightness: Brightness.light,
-          )
-      );
+      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.black,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ));
     }
   }
 
@@ -39,33 +36,40 @@ class ThemeProvider with ChangeNotifier {
       sharedPreferences.setBool(SPref.isLight, false);
       isLightTheme = !isLightTheme;
       notifyListeners();
-    }
-    else {
+    } else {
       sharedPreferences.setBool(SPref.isLight, true);
       isLightTheme = !isLightTheme;
       notifyListeners();
-  }
+    }
     getCurrentNavigationColor();
     notifyListeners();
   }
 
   ThemeData themeData() {
     return ThemeData(
-      brightness: isLightTheme ? Brightness.light : Brightness.dark,
-      scaffoldBackgroundColor: isLightTheme ? Colors.deepOrangeAccent : Colors
-          .black,
-    );
+        brightness: isLightTheme ? Brightness.light : Brightness.dark,
+        scaffoldBackgroundColor:
+            isLightTheme ? Colors.deepOrangeAccent : Colors.black,
+        textTheme: TextTheme(
+            displayLarge: GoogleFonts.stickNoBills(
+                fontSize: 70,
+                fontWeight: FontWeight.w600,
+                color: isLightTheme ? AppColors.black : AppColors.orange),
+            displayMedium: GoogleFonts.robotoCondensed(
+              fontWeight: FontWeight.w500,
+              color: isLightTheme ? AppColors.black : AppColors.orange,
+            )));
   }
 
   ThemeMode themeMode() {
     return ThemeMode(
-      gradientColors: isLightTheme ? [Colors.deepOrange, Colors.orange] : [
-        Colors.black,
-        Colors.black
-      ],
+      gradientColors: isLightTheme
+          ? [Colors.deepOrange, Colors.orange]
+          : [Colors.black, Colors.black],
       switchColor: isLightTheme ? Colors.black : Colors.deepOrange,
-      switchBgColor: isLightTheme ? Colors.black.withOpacity(.1) : Colors.grey
-          .withOpacity(.3),
+      switchBgColor: isLightTheme
+          ? Colors.black.withOpacity(.1)
+          : Colors.grey.withOpacity(.3),
       thumbColor: isLightTheme ? Colors.deepOrange : Colors.black,
     );
   }
@@ -77,6 +81,10 @@ class ThemeMode {
   Color? thumbColor;
   Color? switchBgColor;
 
-  ThemeMode(
-      {this.gradientColors, this.switchColor, this.thumbColor, this.switchBgColor,});
+  ThemeMode({
+    this.gradientColors,
+    this.switchColor,
+    this.thumbColor,
+    this.switchBgColor,
+  });
 }
